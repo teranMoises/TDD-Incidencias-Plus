cb1c9b691f6a20cd7e132ed8322650dec2907995
Moisés Alfonso Terán <teranrivasma@uvm.edu.ve>
2025-04-10 14:11:55 -0400

    icono

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -5,6 +5,7 @@ import './LandingPage.css';
 const LandingPage = () => {
   return (
     <div className="landing-container">
+      {/* <img src={`${process.env.PUBLIC_URL}/logo512-removebg-preview.png`} alt="Landing" className="landing-image" /> */}
       <h1>Bienvenido al Sistema de Gestión de Incidencias</h1>
       <p>Inicia sesión o regístrate para acceder a la aplicación.</p>
       <div className="landing-buttons">
